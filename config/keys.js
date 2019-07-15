797de964becceb91814680756cd5530f7d7adaf3
koman-SA <52891065+koman-SA@users.noreply.github.com>
2019-07-15 09:03:22 -0700

    Change password

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://eliasonw:Exarkun4252!@cluster0-wmkui.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://eliasonw:Exarkun4252@cluster0-wmkui.mongodb.net/test?retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
